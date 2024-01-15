ad592892d2ccd99301351fae2f46d8ec2c6b751d
Teemu Autto <teemu.a.autto@jyu.fi>
2024-01-15 18:23:32 +0000

    Simplify otel setup by removing examples.

--- fileserv/src/app.js
+++ fileserv/src/app.js
@@ -91,17 +91,9 @@ function initTelemetry() {
             // [SemanticResourceAttributes.SERVICE_VERSION]: ,
         }),
         traceExporter: new OTLPTraceExporter({
-            // // optional - default url is http://localhost:4318/v1/traces
-            // url: '<your-otlp-endpoint>/v1/traces',
-            // // optional - collection of custom headers to be sent with each request, empty by default
-            // headers: {},
         }),
         metricReader: new PeriodicExportingMetricReader({
-            exporter: new OTLPMetricExporter({
-                // url: '<your-otlp-endpoint>/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
-                // headers: {}, // an optional object containing custom headers to be sent with each request
-                concurrencyLimit: 1, // an optional limit on pending requests
-            }),
+            exporter: new OTLPMetricExporter({}),
         }),
         instrumentations: [getNodeAutoInstrumentations()],
       });
@@ -143,7 +135,8 @@ function initSentry() {
             new Sentry.Integrations.Express({ app }),
             // Automatically instrument Node.js libraries and frameworks
             ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-        ]
+        ],
+        instrumenter: 'otel',
     });
 
     // RequestHandler creates a separate execution context, so that all
