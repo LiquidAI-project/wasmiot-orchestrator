9d4d06bc40cc92a245aa27a9d21986450a906c10
Ville Heikkilä <ville.heikkila@tuni.fi>
2025-06-03 17:47:13 +0300

    Update test configurations

--- deployment-tests/test1.sh
+++ deployment-tests/test1.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cp .env.template1 .env
+
 # stop any running containers
 docker compose -f docker-compose.test1.yml down
 
@@ -10,8 +12,6 @@ docker volume rm wasmiot-mongo-db-test || true
 docker volume rm wasmiot-mongo-config-test || true
 
 # stop screen sessions running local supervisors
-# TODO: this might leave some processes running, needs to be handled better
-screen -X -S supervisor kill || true
 screen -X -S supervisor kill || true
 screen -X -S supervisor kill || true
 
@@ -22,45 +22,44 @@ then
 fi
 
 # pull the latest images
-docker compose -f docker-compose.test1.yml pull mongo mongo-express mdns-reflector nginx-proxy
+docker compose -f docker-compose.test1.yml pull mongo mongo-express nginx-proxy
 
 # build all images
 docker compose -f docker-compose.test1.yml build
 
-# compile local mdns server
-current_dir=$(pwd)
-cd mdns-server
-cargo build --release
-cd ${current_dir}
 
 # compile local supervisors
+current_dir=$(pwd)
 cd ../wasmiot-supervisor
 python3 -m pip install -r requirements.txt
 cd ../supervisor-rust-port
 cargo build --release
 cd ${current_dir}
 
-# start MongoDB and Mongo Express
-docker compose -f docker-compose.test1.yml up --detach mongo mongo-express
+# start MongoDB, Mongo Express, and mDNS reflector
+docker compose -f docker-compose.test1.yml up --detach --remove-orphans mongo mongo-express mdns-reflector
 
 # wait for 10 seconds to ensure MongoDB is up
+echo "Waiting for 10 seconds to ensure the MongoDB is up..."
 sleep 10
-
-# start the mDNS server with Docker
-docker compose -f docker-compose.test1.yml up --detach mdns-server
 
 # start the orchestrator and the Nginx proxy
-docker compose -f docker-compose.test1.yml up --detach mdns-reflector orchestrator nginx-proxy
+docker compose -f docker-compose.test1.yml up --detach orchestrator nginx-proxy
 
 # wait for 10 seconds to ensure the orchestrator is up
+echo "Waiting for 10 seconds to ensure the orchestrator is up..."
 sleep 10
 
-# start the local mDNS server
-screen -d -m -U -L -Logfile screen.log -S supervisor -t local-mdns ./start_local_mdns-server.sh
-
 # start the local supervisors
-screen -S supervisor -X screen -t local-python ./start_local_supervisor.sh python
+screen -d -m -U -L -Logfile screen.log -S supervisor -t local-python ./start_local_supervisor.sh python
 screen -S supervisor -X screen -t local-rust ./start_local_supervisor.sh rust
 
-# start the Docker Supervisors
+# start the Docker supervisors
+echo "Waiting for 10 seconds to ensure the local supervisors are up..."
+sleep 10
 docker compose -f docker-compose.test1.yml up --detach supervisor-python supervisor-rust
+
+# start the extra Docker supervisors in a separate Docker network
+echo "Waiting for 10 seconds to ensure the Docker supervisors are up..."
+sleep 10
+docker compose -f docker-compose.test1.yml up --detach supervisor-extra-python supervisor-extra-rust
